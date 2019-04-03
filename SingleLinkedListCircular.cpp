217d24c6cd0fc6305ceda7575d516e269ef5be0d
eggybintangn13 <48612244+eggybintangn13@users.noreply.github.com>
2019-04-03 22:15:38 +0700

    Memasukkan depan, belakang, & mencetak

--- SingleLinkedListCircular.cpp
+++ SingleLinkedListCircular.cpp
@@ -1,5 +1,65 @@
 #include <iostream>
 using namespace std;
-int main (){
+struct node{
+  int data;
+  node *next;
+};
+
+node *head=NULL;
+
+bool isEmpty(){
+  return head==NULL;
+}
+
+void depan(int nilai){
+  node *baru;
+  baru=new node;
+  baru->data=nilai;
+  baru->next=NULL;
+  
+  if(isEmpty()){
+    head=baru;
+  }
+  else{
+    baru->next=head;
+    head=baru;
+  }
+}
+
+void belakang(int nilai){
+  node *baru, *bantu;
+  baru=new node;
+  baru->data=nilai;
+  baru->next=NULL;
+  if(isEmpty()){
+    head=baru;
+  }
+  else{
+    bantu=head;
+    while(bantu->next!=NULL){
+      bantu=bantu->next;
+    }
+    bantu->next=baru;
+  }
+}
+
+void cetak(){
+  node *bantu;
+  if(isEmpty()){
+    cout<<"List Kosong";
+  }
+  else{
+    bantu=head;
+    while(bantu!=NULL){
+      cout<<bantu->data<<endl;
+      bantu=bantu->next;
+    }
+  }
+}
+
+ int main (){
+  depan(3);
+  belakang(5);
+  cetak();
   return 0;
 }
